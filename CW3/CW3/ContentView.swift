b7163448786d374df812be560302400b62be3ed8
deemfaisal <dfaisal07@gmail.com>
2022-09-10 21:12:26 +0300

    Project3

---
+++ CW3/CW3/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CW3
+//
+//  Created by Deem on 10/09/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
