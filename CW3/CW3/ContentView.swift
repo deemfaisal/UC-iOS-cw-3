d4b15e1460ac4b4c21613bb2b5ae3a33bbeb4d57
deemfaisal <dfaisal07@gmail.com>
2022-09-11 02:47:14 +0300

    Project3part1 done

--- CW3/CW3/ContentView.swift
+++ CW3/CW3/ContentView.swift
@@ -8,9 +8,99 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var grade = ""
+    @State var g1 = "start1"
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        
+        ZStack {
+            
+            
+            Image("gp1")
+                .resizable()
+                .scaledToFill()
+                .ignoresSafeArea()
+          
+            
+            
+            
+            VStack {
+                
+                
+                Text("Grades Calculator")
+                    .font(.system(size: 40))
+                    .foregroundColor(.black)
+                    .italic()
+                    .bold()
+                    .padding(.bottom)
+                
+        
+                
+               
+                TextField("Enter your grade", text: $grade)
+                    .font(.system(size: 25))
+                    .multilineTextAlignment(.center)
+                    .frame(width: 280, height: 40)
+                    .background(.white)
+                    .cornerRadius(30)
+                    .shadow(radius: 20)
+                    .padding()
+                
+                
+                Text("Calculate")
+                    .font(.system(size: 25))
+                    .foregroundColor(.white)
+                    .frame(width: 150, height: 40)
+                    .background(.black)
+                    .cornerRadius(30)
+                    .shadow(radius: 20)
+                    .padding()
+                    .onTapGesture {
+                        
+                        if ( Int(grade) ?? 0) >= 90 {
+                            g1 = "excellent"
+                       }
+                        else if (Int(grade) ?? 0 ) >= 80 {
+                            g1 = "verygood"
+
+                        }
+                        
+                        else if (Int(grade) ?? 0) >= 70 {
+                            g1 = "good1"
+                        }
+                        
+                        else if (Int(grade) ?? 0 ) >= 60 {
+                            g1 = "poor1"
+                        }
+                        else {
+                            g1 = "fail1"
+                        }
+                        
+                    }
+                
+                Text("Well, your grade is...")
+                    .font(.system(size: 20))
+                    .italic()
+                    .bold()
+                    .padding()
+                
+                Image(g1)
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 230, height: 200, alignment: .top)
+                    .padding()
+              
+             
+                
+                
+                    
+            }
+            
+            Spacer()
+            
+            
+        }
+        
+       
     }
 }
 
